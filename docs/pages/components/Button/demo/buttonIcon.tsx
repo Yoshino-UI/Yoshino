76c3ed1522cdfab57072edeb1d2ef694780b6605
ShanaMaid <uestczeng@gmail.com>
2018-12-28 11:30:51 +0800

    fix: 修复Button文档icon bug

--- docs/pages/components/Button/demo/buttonIcon.tsx
+++ docs/pages/components/Button/demo/buttonIcon.tsx
@@ -11,16 +11,16 @@ export default function() {
       <div
         style={style}
       >
-        <Button type='primary' icon='play'>播放</Button>
-        <Button type='primary' icon='pause'>暂停</Button>
-        <Button type='primary' icon='skip-backward'>上一首</Button>
-        <Button type='primary' icon='skip-forward' tail>下一首</Button>
+        <Button type='primary' icon='md-play'>播放</Button>
+        <Button type='primary' icon='md-pause'>暂停</Button>
+        <Button type='primary' icon='md-skip-backward'>上一首</Button>
+        <Button type='primary' icon='md-skip-forward' tail>下一首</Button>
       </div>
       <div style={{marginTop: 20}}>
-        <Button type='primary' shape='circle' icon='home'/>
-        <Button shape='circle' icon='home'/>
-        <Button type='dashed' shape='circle' icon='home'/>
-        <Button type='danger' shape='circle' icon='home'/>
+        <Button type='primary' shape='circle' icon='md-home'/>
+        <Button shape='circle' icon='md-home'/>
+        <Button type='dashed' shape='circle' icon='md-home'/>
+        <Button type='danger' shape='circle' icon='md-home'/>
       </div>
     </div>
   );
